5ba356203bf4929a327ca71bbca79a56672edbae
daxida <galmaw@hotmail.com>
2024-04-11 22:50:24 +0200

    Complete the perl regex with an example

--- others/text_processing/ja.pl
+++ others/text_processing/ja.pl
@@ -3,10 +3,10 @@
 s/(?<!。)\n//g;    # remove newlines if not after 。
 s/。(?!\n)/。\n/g;  # add newlines after 。 if needed
 
-# (Linux) Create two files i.txt and o.txt
-# copy paste the text in i.txt then run:
+# (Linux) Create two files i.txt and o.txt (the .txt is optional).
+# Copy paste the text in i.txt then run:
 # ./ja.pl < i.txt > o.txt
-# the result will be in o.txt
+# The result will be in o.txt
 
 # Also with xclip
 # xclip -o | ./ja.pl | xclip -selection clipboard
