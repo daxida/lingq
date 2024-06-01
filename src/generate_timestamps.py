c5d8813788251fd230b54b54a2e9e2f864b81c62
daxida <galmaw@hotmail.com>
2024-06-01 22:04:58 +0200

    Refactor generate_timestamps.py

--- src/generate_timestamps.py
+++ src/generate_timestamps.py
@@ -1,12 +1,8 @@
 import asyncio
-from typing import Any, List
+from typing import Any
 
 from lingqhandler import LingqHandler
 from utils import colors
-
-LANGUAGE_CODE = "ja"
-COURSE_ID = "537808"
-SKIP_ALREADY_TIMESTAMPED = True
 
 
 async def check_if_timestamped(handler: LingqHandler, lesson: Any) -> None:
@@ -22,19 +18,21 @@ async def check_if_timestamped(handler: LingqHandler, lesson: Any) -> None:
         lesson["is_timestamped"] = False
 
 
-async def filter_already_timestamped(handler: LingqHandler, lessons: List[Any]) -> List[Any]:
+async def filter_already_timestamped(handler: LingqHandler, lessons: list[Any]) -> list[Any]:
     tasks = [check_if_timestamped(handler, lesson) for lesson in lessons]
     await asyncio.gather(*tasks)
     return [lesson for lesson in lessons if not lesson["is_timestamped"]]
 
 
-async def generate_timestamps(language_code: str, course_id: str) -> None:
+async def _generate_timestamps(
+    language_code: str, course_id: str, skip_already_timestamped: bool
+) -> None:
     async with LingqHandler(language_code) as handler:
         collection_json = await handler.get_collection_json_from_id(course_id)
         assert collection_json is not None
         collection_title = collection_json["title"]
         lessons = collection_json["lessons"]
-        if SKIP_ALREADY_TIMESTAMPED:
+        if skip_already_timestamped:
             lessons = await filter_already_timestamped(handler, lessons)
             if not lessons:
                 print("Everything was already timestamped!")
@@ -44,9 +42,10 @@ async def generate_timestamps(language_code: str, course_id: str) -> None:
         print(f"Generated timestamps for '{collection_title}'.")
 
 
-def main():
-    asyncio.run(generate_timestamps(LANGUAGE_CODE, COURSE_ID))
+def generate_timestamps(language_code: str, course_id: str, skip_already_timestamped: bool):
+    asyncio.run(_generate_timestamps(language_code, course_id, skip_already_timestamped))
 
 
 if __name__ == "__main__":
-    main()
+    # Defaults for manually running this script.
+    generate_timestamps(language_code="ja", course_id="537808", skip_already_timestamped=True)
