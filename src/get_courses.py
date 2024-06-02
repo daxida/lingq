75f4b3b6c703cca6bb67a63474c52e544a30f100
daxida <galmaw@hotmail.com>
2024-06-02 19:20:57 +0200

    Refactor get_courses.py

--- src/get_courses.py
+++ src/get_courses.py
@@ -1,24 +1,13 @@
 import asyncio
 
-from get_lessons import get_lessons
+from get_lessons import _get_lessons
 from lingqhandler import LingqHandler
-from utils import timing  # type: ignore
-
-# NOTE: This reorders your 'Continue studying' shelf.
-# NOTE: 'Attempt to decode JSON with unexpected mimetype: text/html; charset=utf-8'
-#       is probably a timeout issue. Try to slow down the requests by increasing sleep time.
-#       (RARE) It can also be due to a 500 error: Internal server error.
-#       You should check the lesson itself to see if you can open it in the browser.
-
-# get_collections(LANG): download all 'my imported' courses in LANG.
-# get_all_collections(): download all 'my imported' courses in all languages.
-
-LANGUAGE_CODE = "ja"
-DOWNLOAD_AUDIO = False
-SLEEP_TIME = 2
+from utils import double_check, timing  # type: ignore
 
 
-async def get_collections(language_code: str, download_audio: bool) -> None:
+async def _get_courses_for_language(
+    language_code: str, download_audio: bool, sleep_time: int, download_folder: str
+) -> None:
     async with LingqHandler(language_code) as handler:
         # This is only for the courses ID, since in order to get the text we need
         # to do yet another request handled through `get_collection_from_id`
@@ -27,28 +16,60 @@ async def get_collections(language_code: str, download_audio: bool) -> None:
 
         # Async fetching of courses is too fast for the API...
         for collection_json in collections_json:
-            await get_lessons(
+            await _get_lessons(
                 language_code,
                 collection_json["id"],
                 skip_already_downloaded=False,
                 download_audio=download_audio,
+                download_folder=download_folder,
+                verbose=False,
             )
-        await asyncio.sleep(SLEEP_TIME)
+        await asyncio.sleep(sleep_time)
 
 
-async def get_all_collections(download_audio: bool) -> None:
+async def _get_courses(
+    language_codes: list[str], download_audio: bool, sleep_time: int, download_folder: str
+) -> None:
+    print(f"Getting courses for languages: {', '.join(language_codes)}")
+    double_check("CAREFUL: This reorders your 'Continue studying' shelf.")
+    for language_code in language_codes:
+        await _get_courses_for_language(language_code, download_audio, sleep_time, download_folder)
+        await asyncio.sleep(sleep_time)
+
+
+async def _get_all_courses(download_audio: bool, sleep_time: int, download_folder: str) -> None:
     async with LingqHandler("Filler") as handler:
         language_codes = await handler.get_language_codes()
-        for language_code in language_codes:
-            await get_collections(language_code, download_audio)
-            await asyncio.sleep(SLEEP_TIME)
+        await _get_courses(language_codes, download_audio, sleep_time, download_folder)
 
 
 @timing
-def main():
-    asyncio.run(get_collections(LANGUAGE_CODE, DOWNLOAD_AUDIO))
-    # asyncio.run(get_all_collections(DOWNLOAD_AUDIO))
+def get_courses(
+    language_codes: list[str], download_audio: bool, sleep_time: int, download_folder: str
+) -> None:
+    """
+    Get every course from a list of languages.
+
+    CAREFUL: This reorders your 'Continue studying' shelf.
+
+    Args:
+        language_codes (list[str]): List of language codes to process.
+            If no language codes are given, use all languages.
+        download_audio (bool): If True, downloads audio files for the courses.
+        sleep_time (int): The sleep time between requests to prevent timeouts.
+
+    NOTE: 'Attempt to decode JSON with unexpected mimetype: text/html; charset=utf-8'
+          is probably a timeout issue. Try to slow down the requests by increasing sleep time.
+          (RARE) It can also be due to a 500 error: Internal server error.
+          You should check the lesson itself to see if you can open it in the browser.
+    """
+    # By default, if no language codes are given, get all the courses;
+    if not language_codes:
+        asyncio.run(_get_all_courses(download_audio, sleep_time, download_folder))
+    else:
+        asyncio.run(_get_courses(language_codes, download_audio, sleep_time, download_folder))
 
 
 if __name__ == "__main__":
-    main()
+    # Defaults for manually running this script.
+    get_courses(language_codes=["ja"], download_audio=False, sleep_time=2, download_folder=".")
