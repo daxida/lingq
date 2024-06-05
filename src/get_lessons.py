4d4c7c4d588ed9d7cb89268a3d59c8529a15d1a8
daxida <galmaw@hotmail.com>
2024-06-05 17:16:38 +0200

    Refactor get_lessons.py

--- src/get_lessons.py
+++ src/get_lessons.py
@@ -1,12 +1,19 @@
 import asyncio
 import os
+from dataclasses import dataclass
 from typing import Any
 
 from lingqhandler import LingqHandler
 from utils import timing  # type: ignore
 
-# A simple lesson type: (title, text, audio)
-Lesson = tuple[str, list[str], bytes | None]
+
+@dataclass
+class Lesson:
+    title: str
+    url: str
+    id_: str
+    text: str
+    audio: bytes | None
 
 
 def sanitize_title(title: str) -> str:
@@ -18,12 +25,19 @@ async def get_lesson(
 ) -> Lesson:
     title = lesson_json["title"]
     # title = f"{idx}.{lesson['title']}" # add indices
+    url = lesson_json["url"]
+    id_ = lesson_json["id"]
 
-    lesson_text_json = await handler.get_lesson_from_url(lesson_json["url"])
-    text = []
-    for token in lesson_text_json["tokenizedText"]:
-        paragraph = " ".join(line["text"] for line in token)
-        text.append(paragraph)  # type: ignore
+    lesson_text_json = await handler.get_lesson_from_url(url)
+
+    paragraphs: list[str] = []
+    _title, *tokenized_text = lesson_text_json["tokenizedText"]
+    for paragraph_data in tokenized_text:
+        paragraph: list[str] = []
+        for sentence in paragraph_data:
+            paragraph.append(sentence["text"])
+        paragraphs.append(" ".join(paragraph))
+    text = "\n".join(paragraphs)
 
     if download_audio:
         # The audio doesn't need the lesson_text_json
@@ -31,9 +45,9 @@ async def get_lesson(
     else:
         audio = None
 
-    lesson: Lesson = (title, text, audio)
+    lesson = Lesson(title, url, id_, text, audio)
     if verbose:
-        print(f"Downloaded lesson nº{idx}: {lesson_json['title']}")
+        print(f"Downloaded lesson nº{idx}: {title}")
 
     return lesson
 
@@ -51,20 +65,21 @@ def write_lessons(
     os.makedirs(texts_folder, exist_ok=True)
     os.makedirs(audios_folder, exist_ok=True)
 
-    for idx, (title, text, audio) in enumerate(lessons, 1):
-        sanitized_title = sanitize_title(title)
+    for idx, lesson in enumerate(lessons, 1):
+        sanitized_title = sanitize_title(lesson.title)
 
-        if audio:
+        if lesson.audio:
             mp3_path = os.path.join(audios_folder, f"{sanitized_title}.mp3")
             with open(mp3_path, "wb") as audio_file:
-                audio_file.write(audio)
+                audio_file.write(lesson.audio)
 
         txt_path = os.path.join(texts_folder, f"{sanitized_title}.txt")
         with open(txt_path, "w", encoding="utf-8") as text_file:
-            text_file.write("\n".join(text))
+            text_file.write(f"{lesson.title}\n")
+            text_file.write(lesson.text)
 
         if verbose:
-            print(f"Writing lesson nº{idx}: {title}")
+            print(f"Writing lesson nº{idx}: {lesson.title}")
 
 
 async def _get_lessons(
@@ -74,7 +89,7 @@ async def _get_lessons(
     download_audio: bool,
     download_folder: str,
     verbose: bool,
-) -> None:
+) -> tuple[str, list[Lesson]]:
     async with LingqHandler(language_code) as handler:
         collection_json = await handler.get_collection_json_from_id(course_id)
         assert collection_json is not None
@@ -86,21 +101,22 @@ async def _get_lessons(
             if verbose
             else ""
         )
-        print(f"Downloading: '{collection_title}'{at_url}")
+        print(f"Getting: '{collection_title}'{at_url}")
 
-        texts_folder = os.path.join(download_folder, language_code, collection_title, "texts")
-        if skip_already_downloaded and os.path.exists(texts_folder):
-            text_files = os.listdir(texts_folder)
-            filtered_lessons = [
-                lesson
-                for lesson in lessons
-                if f"{sanitize_title(lesson['title'])}.txt" not in text_files
-            ]
-            if verbose:
-                print(
-                    f"Skipped {len(lessons) - len(filtered_lessons)} out of {len(lessons)} lessons that were already downloaded."
-                )
-            lessons = filtered_lessons
+        if skip_already_downloaded:
+            texts_folder = os.path.join(download_folder, language_code, collection_title, "texts")
+            if os.path.exists(texts_folder):
+                text_files = os.listdir(texts_folder)
+                filtered_lessons = [
+                    lesson
+                    for lesson in lessons
+                    if f"{sanitize_title(lesson['title'])}.txt" not in text_files
+                ]
+                if verbose:
+                    print(
+                        f"Skipped {len(lessons) - len(filtered_lessons)} out of {len(lessons)} lessons that were already downloaded."
+                    )
+                lessons = filtered_lessons
 
         tasks = [
             get_lesson(handler, lesson_json, idx, download_audio, verbose)
@@ -108,7 +124,7 @@ async def _get_lessons(
         ]
         lessons = await asyncio.gather(*tasks)
 
-        write_lessons(language_code, collection_title, lessons, download_folder, verbose)
+        return collection_title, lessons
 
 
 @timing
@@ -132,7 +148,7 @@ def get_lessons(
 
     Creates a 'download' folder and saves the text/audio in 'text'/'audio' subfolders.
     """
-    asyncio.run(
+    collection_title, lessons = asyncio.run(
         _get_lessons(
             language_code,
             course_id,
@@ -142,6 +158,7 @@ def get_lessons(
             verbose,
         )
     )
+    write_lessons(language_code, collection_title, lessons, download_folder, verbose)
 
 
 if __name__ == "__main__":
