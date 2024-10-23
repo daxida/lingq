7806c9c9d1c7bf694d7507f608fa50b645eaacae
daxida <galmaw@hotmail.com>
2024-10-23 11:07:49 +0200

    Add simple test for yomitan

---
+++ tests/test_yomitan.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+
+from yomitan import yomitan
+
+
+def _rewrite_json_with_first_n_entries(json_file_path: Path, n: int = 5) -> None:
+    with json_file_path.open("r", encoding="utf-8") as f:
+        data = json.load(f)
+        first_ten_entries = data[:n]
+    with json_file_path.open("w", encoding="utf-8") as f:
+        json.dump(first_ten_entries, f, ensure_ascii=False, indent=4)
+
+
+def test_yomitan():
+    """Simply test that the yomitan function does not crash."""
+    # TODO: only use path
+    fixture_path_str = "tests/fixtures/lingqs"
+    fixture_path = Path(fixture_path_str)
+    assert (fixture_path / "pt").exists()
+
+    zip_file_path = fixture_path / "pt" / "lingqs-pt.zip"
+    assert not zip_file_path.exists()
+
+    yomitan(["pt"], fixture_path_str)
+    assert zip_file_path.exists()
+
+    zip_file_path.unlink()
+    assert not zip_file_path.exists()
+
+
+if __name__ == "__main__":
+    # Generate fixture
+    json_file = Path("downloads/lingqs/pt/lingqs.json")
+    _rewrite_json_with_first_n_entries(json_file)
